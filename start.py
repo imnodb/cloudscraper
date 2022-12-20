591e53ca84a0b6c09d9add1d1f8b5770a0de80c8
DB Eriospermum <imnodb@gmail.com>
2022-12-20 14:55:11 +0800

    Create start.py

---
+++ start.py
@@ -0,0 +1,5 @@
+import cloudscraper
+
+scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+# Or: scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
+print(scraper.get("https://opensea.io/0x51b0").text)  # => "<!DOCTYPE html><html><head>..."
